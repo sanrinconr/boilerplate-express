b9bbb29e1b340eb85d29dab79c1dba8e7a65cd5b
Santiago Rincon <santiagorincon2001@gmail.com>
2021-03-31 10:27:22 -0500

    Bind port

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const app = require('./src/app');
 
 let server;
-  server = app.listen(3001, () => {
+  server = app.listen(process.env.PORT | 3001, () => {
     console.log(`Listening to port ${3001}`);
 });
